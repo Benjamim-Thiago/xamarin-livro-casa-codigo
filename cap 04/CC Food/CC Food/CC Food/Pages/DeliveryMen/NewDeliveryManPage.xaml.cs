2360ca93da93f219a62a97ea7033058efa037d1c
Benjamim Thiago <bebenjamimthiago@gmail.com>
2021-03-02 10:23:19 -0300

    feito cadastro de entregadores

--- cap 04/CC Food/CC Food/CC Food/Pages/DeliveryMen/NewDeliveryManPage.xaml.cs
+++ cap 04/CC Food/CC Food/CC Food/Pages/DeliveryMen/NewDeliveryManPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CC_Food.dao;
+using CC_Food.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,38 @@ namespace CC_Food.Pages.DeliveryMen
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class NewDeliveryManPage : ContentPage
     {
+        private DeliveryManDAL deliveryMen = DeliveryManDAL.GetInstance();
         public NewDeliveryManPage()
         {
             InitializeComponent();
+            PrepareNewDeliveryMan();
+        }
+
+        public void BtnGravarClick(object sender, EventArgs e)
+        {
+            if (name.Text.Trim() == string.Empty || phoneNumber.Text == string.Empty)
+            {
+                this.DisplayAlert("Erro", "Você precisa informar o nome e telefone para o novo entregador.", "Ok");
+            }
+            else
+            {
+                deliveryMen.Add(new DeliveryMan()
+                {
+                    id = Convert.ToUInt32(id.Text),
+                    name = name.Text,
+                    phoneNumber = phoneNumber.Text
+                });
+                PrepareNewDeliveryMan();
+            }
+        }
+
+        private void PrepareNewDeliveryMan()
+        {
+            var novoId = deliveryMen.GetAll().Max(x => x.id)
+            + 1;
+            id.Text = novoId.ToString().Trim();
+            name.Text = string.Empty;
+            phoneNumber.Text = string.Empty;
         }
     }
 }
